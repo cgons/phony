554676b6a4f6cda4a65a5c65ce2909e3cc051f51
cgons <cgons@pcxchange.ca>
2018-05-16 23:28:50 -0400

    Refactor core/route_test.go to use Go's subtest structure.

--- core/route_test.go
+++ core/route_test.go
@@ -4,20 +4,22 @@ import (
 	"testing"
 )
 
-func TestGetStatus_Returns200WhenStatusNotSet(t *testing.T) {
-	expectedStatus := 200
-	route := Route{}
-	result := route.GetStatus()
-	if result != expectedStatus {
-		t.Fatalf("Expected %d, got %d", expectedStatus, result)
-	}
-}
+func TestGetStatus(t *testing.T) {
+	t.Run("Returns200WhenStatusNotSet", func(t *testing.T) {
+		expectedStatus := 200
+		route := Route{}
+		result := route.GetStatus()
+		if result != expectedStatus {
+			t.Fatalf("Expected %d, got %d", expectedStatus, result)
+		}
+	})
 
-func TestGetStatus_ReturnsStatusWhenSet(t *testing.T) {
-	expectedStatus := 400
-	route := Route{Status: 400}
-	result := route.GetStatus()
-	if result != expectedStatus {
-		t.Fatalf("Expected %d, got %d", expectedStatus, result)
-	}
+	t.Run("ReturnsStatusWhenSet", func(t *testing.T) {
+		expectedStatus := 400
+		route := Route{Status: 400}
+		result := route.GetStatus()
+		if result != expectedStatus {
+			t.Fatalf("Expected %d, got %d", expectedStatus, result)
+		}
+	})
 }
